4c46af2e5bfe1fe112ae42a0d62874d07080768a
madhanrajR <madhanit31@gmail.com>
2018-03-08 15:09:20 +0530

    Create Erc20.sol

---
+++ Erc20.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.0;
+contract Token {
+    function mint(uint256 _amount);
+     function balanceOf(address _owner)public constant returns (uint256 balance); 
+     function transfer(address to,uint256 amount); 
+     
+     }
